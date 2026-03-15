3292ab82a46cb9f281655929844a0e2c8cc01873
pnietohurtado <pnietohurtado03@gmail.com>
2026-03-15 12:03:58 +0100

    Pestaña de calendario completamente funcional

--- Android_Ovulitos/app/src/main/java/com/example/ovulitos/CalendarioFragment.java
+++ Android_Ovulitos/app/src/main/java/com/example/ovulitos/CalendarioFragment.java
@@ -1,7 +1,7 @@
 package com.example.ovulitos;
 
+import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,23 +10,30 @@ import android.widget.CalendarView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.util.Pair;
 import androidx.fragment.app.Fragment;
 
-import com.example.ovulitos.currentUser.UserData;
-import com.google.android.material.datepicker.MaterialDatePicker;
-import com.google.firebase.Firebase;
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.time.Instant;
+import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class CalendarioFragment extends Fragment {
 
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private String fechaSeleccionada = "";
+    private DateTimeFormatter dateParser;
+
+    private ChipGroup flujo;
+    private ChipGroup sintomas;
+    private String selectedOptions1;
+    private String selectedOptions2;
 
     @Nullable
     @Override
@@ -41,9 +48,49 @@ public class CalendarioFragment extends Fragment {
 
         CalendarView calendarView = view.findViewById(R.id.calendarViewRegistro);
         Button btnGuardar = view.findViewById(R.id.btnGuardarRegistro);
+        ChipGroup flujo = view.findViewById(R.id.chipGroupFlujo);
+        ChipGroup sintomas = view.findViewById(R.id.chipGroupSintomas);
 
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            this.dateParser = DateTimeFormatter.ofPattern("dd/MMMM/yyyy");
+            this.fechaSeleccionada = Instant.ofEpochMilli(calendarView.getDate())
+                    .atZone(ZoneId.systemDefault())
+                    .format(dateParser);
+        }
 
+        calendarView.setOnDateChangeListener((view1, year, month, dayOfMonth) -> {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                LocalDate localDate = LocalDate.of(year, month + 1, dayOfMonth);
+                DateTimeFormatter dateParser = DateTimeFormatter.ofPattern("dd/MMMM/yyyy");
+                this.fechaSeleccionada = localDate.format(dateParser);
+
+                // Con esto pretendo cambiar la fecha seleccionada dentro del calendario, pero se rompe todo el rato
+                //calendarView.setDate(Long.parseLong(this.fechaSeleccionada));
+                //calendarView.setDate(Long.parseLong(this.fechaSeleccionada), true, true);
+            }
+        });
+
+        btnGuardar.setOnClickListener(v -> {
+
+
+            if (!fechaSeleccionada.isEmpty()) {
+                int checked = flujo.getCheckedChipId();
+                Chip chip = flujo.findViewById(checked);
+
+                /* Esta parte da error
+                int checked2 = sintomas.getCheckedChipId();
+                Chip chip2 = sintomas.findViewById(checked2);
+                */
+                userData(fechaSeleccionada, chip.getText().toString(), "Colicos" );
+                Toast.makeText(getContext(), "Guardando: " + fechaSeleccionada, Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getContext(), "Por favor selecciona una fecha", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        /* Toda esta es la lógica que debe de estar en el calendario del registro
 
         MaterialDatePicker<Pair<Long, Long>> dateRangePicker =
                 MaterialDatePicker.Builder.dateRangePicker()
@@ -70,22 +117,35 @@ public class CalendarioFragment extends Fragment {
                                 .format(dateParser);
                     }
 
-                    /*La única manera de de poder hacer que los datos pasen a la base de datos es mediante enviándolos
-                    * según el usuario los marca*/
+                    //La única manera de de poder hacer que los datos pasen a la base de datos es mediante enviándolos
+                    // según el usuario los marca
                     userDataStore(start, end);
 
         });
 
         dateRangePicker.show(getParentFragmentManager(), "DATE_PICKER");
 
-        //botón guardar
-        btnGuardar.setOnClickListener(v -> {
-            Toast.makeText(getContext(), UserData.getUsuario(), Toast.LENGTH_SHORT).show();
-        });
+        */
+
+
+    }
+
+    private void userData(String fechaSelected, String option1, String option2){
+        Map<String, Object> user = new HashMap<>();
+        user.put("fechaSelected", fechaSelected);
+        user.put("intensidad del sangrado", option1);
+        user.put("Síntomas", option2);
+
+        db.collection("usuarios").document("andreaorpez@gmail.com").collection("Datos")
+                .document("datosCalendario").set(user)
+
+                .addOnFailureListener(e ->
+                        Toast.makeText(getContext(), "Problema cargando los datos del usuario!", Toast.LENGTH_SHORT).show()
+                );
     }
 
 
-
+    /*
     private void userDataStore(String  start, String end){
         Map<String, Object> userData = new HashMap<>();
         userData.put("startPeriod", start);
@@ -99,5 +159,5 @@ public class CalendarioFragment extends Fragment {
                 .addOnFailureListener(e ->
                         Log.e("FIRESTORE", "Error guardando datos: " + e.getMessage())
                 );
-    }
+    }*/
 }
